8993d227b60fce93c21c0dfa960cc9397e7335da
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2020-12-02 21:54:42 +0200

    Initial commit

---
+++ ReverseString/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Zsombor
+ * Date: 2020-12-02
+ * Time: 21:20
+ */
+using System;
+
+namespace reverse_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a text: ");
+            string text = Console.ReadLine();
+            ReverseString(text);
+            Console.WriteLine(text);
+        }
+
+        static string ReverseString(string input) {
+            if (string.IsNullOrEmpty(input)) {
+                return string.Empty;
+            }
+            
+            string reversed = "";
+
+            int length = input.Length;
+            StringBuilder reversed = new Stringbuilder();
+
+            while (index >= 0)
+            {
+                //Console.WriteLine($"{text[index]}");
+                //reversed += $"{text[index]}";
+                reversed.Append($"{input[index]}");
+                index--;
+            }
+            return reversed.ToString();
+        }
+    }
+}
